cbe28ff5e3421901b3489159af802588a32aff35
Gordon Stewart <gstew5@gmail.com>
2018-02-14 15:59:10 -0500

    Work with William in tutorial

--- FollowthePerturbedLeader.cpp
+++ FollowthePerturbedLeader.cpp
@@ -18,28 +18,42 @@ const double EPSILON = 0.5; //Constant used in generating geometric distribution
 
 vector<pair<double, double> > opponent(); //Generates uniformly distributed costs
 
-vector<pair<double, double> > Xa(); //Generates geometrically distributed bonuses
+pair<double, double> Xa(); //Generates geometrically distributed bonuses
 
 int main()
 {
 	vector<pair<double, double> > costs = opponent();
-	vector<pair<double, double> > bonuses = Xa();
+	pair<double, double> bonuses = Xa();
 	vector<pair<string, double> > choices;
 	double red_total = 0.0;
 	double blue_total = 0.0;
+	double total = 0.0;
 	ofstream outStream;
+
+	cout << "Red bonus = " << bonuses.first << endl;
+	cout << "Blue bonus = " << bonuses.second << endl;
+
+	costs[0].first -= bonuses.first;
+	costs[0].second -= bonuses.second;
+
 	for (size_t i = 0; i < CHOICES; i++)
 	{
 		red_total += costs[i].first;
 		blue_total += costs[i].second;
-		if (red_total - bonuses[i].first < blue_total - bonuses[i].second)
+		if (red_total < blue_total)
 		{
-			choices.push_back(make_pair("red", costs[i].first));
+		  total += costs[i].first;
+		  choices.push_back(make_pair("red", costs[i].first));
 		}
 		else
 		{
-			choices.push_back(make_pair("blue", costs[i].second));
+		  total += costs[i].second;		  
+                  choices.push_back(make_pair("blue", costs[i].second));
 		}
+		cout << "Total = " << total << endl;
+		cout << "Total cost of Red = " << red_total << endl;;
+		cout << "Total cost of Blue = " << blue_total << endl;
+		
 	}
 	outStream.open("Actions.txt");
 	for (size_t i = 0; i < CHOICES; i++)
@@ -50,21 +64,17 @@ int main()
     return 0;
 }
 
-vector<pair<double, double> > Xa()
+pair<double, double> Xa()
 {
-	vector<pair<double, double> > bonuses;
+	pair<double, double> bonuses;
 	double red, blue;
 	time_t timer;
 	time(&timer);
 	default_random_engine generator(timer);
-	geometric_distribution<double> distribution(EPSILON);
-	for (int i = 0; i < CHOICES; i++)
-	{
-		red = distribution(generator);
-		blue = distribution(generator);
-		bonuses.push_back(make_pair(red, blue));
-	}
-	return bonuses;
+	uniform_real_distribution<double> distribution(0.0, 1/EPSILON);
+	red = distribution(generator);
+	blue = distribution(generator);
+	return make_pair(red, blue);
 }
 
 vector<pair<double, double> > opponent()
@@ -73,7 +83,7 @@ vector<pair<double, double> > opponent()
 	time(&timer);
 	vector<pair<double, double> > moves;
 	default_random_engine generator(timer);
-	uniform_real_distribution<double> distribution(0.0, 2.0);
+	uniform_real_distribution<double> distribution(0.0, 1.0);
 	pair<double, double> choices;
 	double red;
 	double blue;
