1851ff849d2b539d20a994c21ea96634be2397e1
WilliamKanieski <36244401+WilliamKanieski@users.noreply.github.com>
2018-04-18 07:45:30 -0400

    Add files via upload

--- FTLvsFTPLVersion4.cpp
+++ FTLvsFTPLVersion4.cpp
@@ -1,7 +1,7 @@
 // FTLvsFTPLVersion4.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+//#include "stdafx.h"
 #include <iostream>
 #include <cstdlib>
 #include <string>
@@ -16,7 +16,7 @@ const int CHOICES = 1000; //Total number of iterations of the loop
 
 const double EPSILON = 0.5;
 
-vector<vector<double> > opponent(int number_choices); //Set of costs created by the self-aware opponent
+vector<vector<double> > opponent(size_t number_choices); //Set of costs created by the self-aware opponent
 
 int main()
 {
@@ -24,7 +24,7 @@ int main()
 	ofstream outs;
 	outs.open("Actions.txt"); //Opens file to write to
 	char have_bonuses;
-	int number_choices;
+	size_t number_choices;
 	cout << "How many choices do you want?" << endl;
 	cin >> number_choices;
 	cout << "Do you want to add bonuses? (Y/N)" << endl; //Decides whether or not bonuses will be used
@@ -86,25 +86,30 @@ int main()
 	outs << "----------------------------------------------------" << endl << endl;
 
 	size_t min_index;
-	for (size_t i = 1; i < CHOICES; i++)
+	for (size_t i = 1; i < CHOICES; i++) //Zero was added in for debugging, was originally a one
 	{
 		for (size_t j = 0; j < number_choices; j++)
 		{
 			player_totals[j] += costs[i][j];
+			cout << costs[i][j] << " "; //debugging
 		}
 		min_index = 0;
 		for (size_t j = 0; j < number_choices; j++)
 		{
-			if (player_totals[i] < player_totals[min_index])
+			if (player_totals[j] < player_totals[min_index])
 			{
 				min_index = j;
 			}
 		}
-		total_player_cost += costs[i][min_index];
+		cout << " : " << costs[i][min_index] << " : " << min_index;
+		cout << endl; //debugging
+		total_player_cost += costs[i][(min_index - 1) % number_choices];
+		outs << costs[i][min_index] << endl;
 		outs << "Round " << i << ": " << endl;
 		outs << "Player's choice: " << min_index << endl;
 		outs << "Player's total payment: " << total_player_cost << endl;
-		outs << "Player's total regret: " << total_player_cost - (i / (number_choices)+i % (number_choices)* EPSILON / (number_choices - 1)) << endl;
+		outs << "Player's total regret: " << total_player_cost - (i / number_choices + i % number_choices * EPSILON / (number_choices - 1)) << endl;
+		outs << i / number_choices + i % number_choices * EPSILON / (number_choices - 1) << endl;
 		outs << "----------------------------------------------------" << endl << endl;
 	}
 
@@ -113,7 +118,7 @@ int main()
     return 0;
 }
 
-vector<vector<double> > opponent(int number_choices)
+vector<vector<double> > opponent(size_t number_choices)
 {
 	vector<vector<double> > opponent(CHOICES);
 	for (size_t i = 0; i < CHOICES; i++)
@@ -127,14 +132,22 @@ vector<vector<double> > opponent(int number_choices)
 	for (size_t i = 0; i < number_choices; i++) //first cost vector where opponent tries to manipulate player's future choices
 	{
 		opponent[0][i] = i * EPSILON / (number_choices - 1);
-		cout << opponent[0][i] << endl;
+		//cout << opponent[0][i] << endl;
 	}
 	int suspected_choice = 0;
 	for (size_t i = 1; i < CHOICES; i++) //opponent puts ones for the costs it thinks the player will choose and makes the rest zeros
 	{
 		opponent[i][suspected_choice] = 1;
-		cout << suspected_choice << endl;
+		//cout << suspected_choice << endl;
 		suspected_choice = (suspected_choice + 1) % number_choices;
+	}
+	for (size_t i = 0; i < CHOICES; i++) //Debugging
+	{
+		for (size_t j = 0; j < number_choices; j++)
+		{
+			cout << opponent[i][j] << " ";
+		}
+		cout << endl;
 	}
 	return opponent;
 }
